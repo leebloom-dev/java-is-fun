d40aa35e2a4aefe120daea61f7d37ba3bc4b3809
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-08 10:53:45 -0400

    x

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -6,16 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Instantiate a Date Object
-        Date date = new Date();
-
-        // Display time and date using toString()
-        /*
-        * String toString()
-        * Convert invoking Date object to a string and returns
-        * the result.
-        * */
-        System.out.println(date.toString());
 
     }
 
