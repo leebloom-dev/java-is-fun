9fb61093ba56e27a501190b5a73129b86e852e92
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-08 10:43:10 -0400

    add current data time

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,8 +1,22 @@
 package practice;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        // Instantiate a Date Object
+        Date date = new Date();
+
+        // Display time and date using toString()
+        /*
+        * String toString()
+        * Convert invoking Date object to a string and returns
+        * the result.
+        * */
+        System.out.println(date.toString());
+
     }
+
 }
