220266ac8c2a0dff91e7173dd538d5b7e7cc4a85
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-08 10:52:09 -0400

    modified current date and time to its only class

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,21 +1,13 @@
 package practice;
 
-import java.util.Date;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        // Instantiate a Date Object
-        Date date = new Date();
-
-        // Display time and date using toString()
-        /*
-        * String toString()
-        * Convert invoking Date object to a string and returns
-        * the result.
-        * */
-        System.out.println(date.toString());
+        // TODO: Display current date and time
+        CurrentDataAndTime currentDataAndTime = new CurrentDataAndTime();
+        System.out.println("CURRENT DATE AND TIME:");
+        currentDataAndTime.display();
 
     }
 
