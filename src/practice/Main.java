cc99f5c103b709f8b93a65e64abe7d0417795365
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-09 12:15:13 -0400

    initial commit

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // TODO: Create a string
-        // TODO: Create an array of strings
-        // TODO: Find the length of a string
-        // TODO: Concatenate Strings
-        // TODO: Create an algorithm to prove string is palindrome
+        
 
     }
 
