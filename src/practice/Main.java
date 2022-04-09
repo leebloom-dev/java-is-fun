d485c2d473fbc8dee8f975d40ca6e251b37d0b9c
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-09 18:00:59 -0400

    starter

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -3,18 +3,7 @@ package practice;
 public class Main {
 
     public static void main(String[] args) {
-        String lineBreak = "\n---------------------\n";
-
-        // TODO: Display current date and time
-        CurrentDataAndTime currentDataAndTime = new CurrentDataAndTime();
-        System.out.println("DATE AND TIME:");
-        currentDataAndTime.display();
-        System.out.println(lineBreak);
-
-        // TODO: Display formatted date and time
-        DateFormatting dateFormatting = new DateFormatting();
-        System.out.println("FORMATTED DATE AND TIME:");
-        dateFormatting.display();
+        
 
     }
 
