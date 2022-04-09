5b5204c50748821dbe5264d1a2b11d22603bf812
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-09 13:02:03 -0400

    string arrays

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -4,8 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        // TODO: 1. Create a string
+        // TODO: 2. Create an array of strings
+        // TODO: 3. Find the length of a string
+        // TODO: 4. Concatenate Strings
+        // TODO: 5. Create an algorithm to prove string is palindrome
 
+        // Part 1
+        YokohamaHighSchool yokohamaHighSchool = new YokohamaHighSchool();
+//        yokohamaHighSchool.setStudent("Nagato");
+//        System.out.println(yokohamaHighSchool.getStudent());
+
+        // Part 2
+        yokohamaHighSchool.createRoster();
+        yokohamaHighSchool.displayClassRoster();
     }
 
 }
--- src/practice/YokohamaHighSchool.java
+++ src/practice/YokohamaHighSchool.java
@@ -1,11 +1,65 @@
 package practice;
 
+import java.util.Scanner;
+
 public class YokohamaHighSchool {
 
-    // TODO: Create a string
-    // TODO: Create an array of strings
-    // TODO: Find the length of a string
-    // TODO: Concatenate Strings
-    // TODO: Create an algorithm to prove string is palindrome
+    /* FIELDS */
+    private String student;
+    private String[] classRoster = new String[10];
+
+    /* CONSTRUCTORS */
+    public YokohamaHighSchool() {
+    }
+
+    /* METHODS: GETTERS AND SETTERS */
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String[] getClassRoster() {
+        return classRoster;
+    }
+
+    public void setClassRoster(String[] classRoster) {
+        this.classRoster = classRoster;
+    }
+
+    /* METHODS */
+    public void createRoster() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Student Names.");
+        System.out.println("Enter 'x' to quit.");
+
+        // Get student names
+        for (int i = 0; i < classRoster.length; i++) {
+
+            System.out.print("Enter name for student " + i + ": ");
+            String name = input.next();
+
+            if (name.equals("x")) {
+                break;
+            }
+
+        }
+
+        // Close input stream
+        input.close();
+
+    }
+
+    public void displayClassRoster() {
+
+        for (String name : classRoster) {
+            System.out.println(name);
+        }
+
+    }
 
 }
