7de07fb8ef870cb33988b62acd7748fe67fa5b6b
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-12 12:11:24 -0400

    Initial Commit

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // How to find a digit string from alphanumeric string
-        RegexMatches regexMatches = new RegexMatches();
-        regexMatches.display();
 
     }
     
