028f983b8e3407873f54e73be9d1d4c8dbabd2bc
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-13 13:36:28 -0400

    notes added

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,18 +1,9 @@
 package practice;
 
-import practice.controller.CalculatorController;
-import practice.model.CalculatorModel;
-import practice.view.CalculatorView;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        CalculatorView theView = new CalculatorView();
-        CalculatorModel theModel = new CalculatorModel();
-        CalculatorController theController = new CalculatorController(theView, theModel);
-
-        theView.setVisible(true);
         
     }
     
