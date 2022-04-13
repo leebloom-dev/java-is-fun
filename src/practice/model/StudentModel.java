22c687f31a4d83868d371faa45f3acde3c123559
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-13 13:44:02 -0400

    model added

---
+++ src/practice/model/StudentModel.java
@@ -0,0 +1,26 @@
+package practice.model;
+
+public class StudentModel {
+
+    /* FIELDS */
+    private String name;
+    private String roll;
+
+    /* METHODS: GETTERS AND SETTERS */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        this.name = aName;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String aRoll) {
+        this.roll = aRoll;  
+    }
+
+}
