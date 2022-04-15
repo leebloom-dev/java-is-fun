16ccd8fdda507bbc0937b3f142a272110ec948e8
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-15 16:58:57 -0400

    static method

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -2,7 +2,17 @@ package practice;
 
 public class Main {
 
+    private static void myStaticInHouseMessage() {
+        System.out.println("This static method came from class Main.");
+    }
+
     public static void main(String[] args) {
+
+        // Static method from class Utilities
+        Utilities.myStaticMethod();
+
+        // Static method from class Main
+        myStaticInHouseMessage();
 
     }
 
--- src/practice/Utilities.java
+++ src/practice/Utilities.java
@@ -1,4 +1,9 @@
 package practice;
 
 public class Utilities {
+
+    protected static void myStaticMethod() {
+        System.out.println("This static method came from class Utilites.");
+    }
+
 }
