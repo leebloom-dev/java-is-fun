3fd063a66932d94e84a2355f8d3e1c08a868dd59
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-17 14:12:56 -0400

    x

--- src/practice/view/AttendanceView.java
+++ src/practice/view/AttendanceView.java
@@ -4,12 +4,6 @@ import java.util.HashMap;
 
 public class AttendanceView {
 
-    public void printStudents(HashMap<String, String> students) {
-        System.out.println("CLASS ROSTER");
-        System.out.println("-------------");
-        for (String id : students.keySet()) {
-            System.out.println("ID: " + id + " Name: " +  students.get(id));
-        }
-    }
+    
 
 }
